25cd0175a349c3bcca53eb65799406ae28c6bea9
TheLastResort <camille.colimard@gmail.com>
2025-05-15 14:31:51 +0200

    fixing run script

--- launch/launch.py
+++ launch/launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
 
     config = os.path.join(
         get_package_share_directory('camera_manager'),
-        'config'
+        'config',
         'camera.yaml'
         )
         
